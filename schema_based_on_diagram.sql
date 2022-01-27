e92b67051fcb135ddd525408c19736824c1713c7
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-01-27 17:07:26 -0500

    Create medical histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,8 +1,15 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE `patients` (
-  `id` int,
-  `name` varchar,
-  `date_of_birth` date,
-  PRIMARY KEY (`id`)
+CREATE TABLE patients (
+  id int,
+  name varchar,
+  date_of_birth date,
+  PRIMARY KEY (id)
+);
+CREATE TABLE medical_histories (
+  id int,
+  admitted_at timestamp,
+  patient_id int,
+  status varchar,
+  PRIMARY KEY (id),
+  FOREIGN KEY (patient_id) REFERENCES patients(id)
 );
