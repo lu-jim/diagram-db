de3dfa413fc46978ae694abbae1d12827bc7efec
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-01-27 17:08:59 -0500

    Add fk contraint to histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,15 +1,17 @@
 /* Database schema to keep the structure of entire database. */
-CREATE TABLE patients (
-  id int,
-  name varchar,
-  date_of_birth date,
-  PRIMARY KEY (id)
+CREATE TABLE "patients" (
+  "id" int,
+  "name" varchar,
+  "date_of_birth" date,
+  PRIMARY KEY ("id")
 );
-CREATE TABLE medical_histories (
-  id int,
-  admitted_at timestamp,
-  patient_id int,
-  status varchar,
-  PRIMARY KEY (id),
-  FOREIGN KEY (patient_id) REFERENCES patients(id)
+CREATE TABLE "medical_histories" (
+  "id" int,
+  "admitted_at" timestamp,
+  "patient_id" int,
+  "status" varchar,
+  PRIMARY KEY ("id"),
+  CONSTRAINT "FK_medical_histories.patient_id"
+    FOREIGN KEY ("patient_id")
+      REFERENCES "patients"("id")
 );
